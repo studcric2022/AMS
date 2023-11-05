6bd2f52733ae8179de58cbafbbf6057f7e20c6b3
ROHIT KUMAR SINGH <128765237+studcric2022@users.noreply.github.com>
2023-11-05 10:44:55 +0530

    Add files via upload

---
+++ vehicle/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Customer,Mechanic,Request,Attendance,Feedback
+admin.site.register(Customer)
+admin.site.register(Mechanic)
+admin.site.register(Request)
+admin.site.register(Attendance)
+admin.site.register(Feedback)
+# Register your models here.
+
